61f3cd4ec6466aa4a3b8d6bbe6db43bbabfb6fd5
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-10-01 16:39:58 +1000

    Example of diamond multiple inheritance

---
+++ diamond.py
@@ -0,0 +1,25 @@
+"""Example of a diamond multiple-inheritance class hierarchy"""
+
+class A:
+    def foo(self):
+        print("A")
+
+class B1(A):
+    def foo(self):
+        print("B1")
+
+class B2(A):
+    def foo(self):
+        print("B2")
+
+class C(B1, B2):
+    pass
+
+a = A()
+a.foo()
+b1 = B1()
+b1.foo()
+b2 = B2()
+b2.foo()
+c = C()
+c.foo()
